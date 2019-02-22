2fe932ab4812a032bd792fcea80ecefa677bce66
megangoodland <megangoodland@gmail.com>
2019-02-22 17:00:58 -0500

    Create ode_solving.cpp
    Basic code written based off of lorentz system example: https://headmyshoulder.github.io/odeint-v2/examples.html

---
+++ ode_solving.cpp
@@ -0,0 +1,51 @@
+// ode_solving.cpp
+
+// Differential equations describing populations in an apartment building of 500 people infested by zombies:
+// dS/dt = - BSZ - ESK
+// dK/dt = - CKZ + ESK
+// dZ/dt = BSZ + CKZ - AKZ
+
+// Want to numerically solve the set of ODEs for several values of Z0 to determine the number of Z0 above which
+// the zombies will win. 
+// Outputs a netcdf file that contains (S, K, Z) population vs. time for two values of the initial Z0 values:
+// one in which the zombies win (everyone is zombies), and one in which they all disappear (Z<1). 
+
+
+#include <iostream>
+#include <boost/numeric/odeint.hpp>
+#include <boost/array.hpp>
+using namespace boost::numeric::odeint;
+using namespace std;
+
+
+// defining constants and initial conditions
+const double A = 0.03;  // rate at which zombies are killed by K
+const double B = 0.02;  // rate at which regular people are turned into Z
+const double C = 0.03;  // rate as which zombie killers are turned into Zombies
+const double E = 0.015; // rate at which zombie killers teach regular people how to kill zombies
+const double K0 = 9; // number of people who can kill zombies
+double Z0 = 5; // number of zombies: will be varying this value
+double S0 = 491 - Z0; // number of regular people who can't kill zombies
+
+
+typedef boost::array< double , 3 > state_type;
+
+void print( const state_type &x , const double t )
+{
+    cout << t << '\t' << x[0] << '\t' << x[1] << '\t' << x[2] << endl;
+}
+
+// x = ( S, K, Z )
+void zombie_odes( const state_type &x , state_type &dxdt , double t )
+{
+    dxdt[0] = -(B*x[0]*x[2])-(E*x[0]*x[2]);
+    dxdt[1] = -(C*x[1]*x[2])+(E*x[0]*x[1]);
+    dxdt[2] = -(B*x[0]*x[2])+(C*x[1]*x[2])-(A*x[1]*x[2]);
+}
+
+
+int main() { 
+  state_type x = {S0, K0, Z0}; // initial conditions
+  // integrate needs (system, x0, t0, t1, dt, observer)
+  integrate( zombie_odes , x , 0.0 , 25.0 , 0.1 , print );
+}
